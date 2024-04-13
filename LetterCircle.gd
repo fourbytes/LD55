307a8d88e6e3cff047f5391e61fe30003049bc52
Oscar Rainford <oscar@fourbs.com.au>
2024-04-13 13:24:03 +1000

    render letter images instead of blank squares.

--- LetterCircle.gd
+++ LetterCircle.gd
@@ -1,26 +1,38 @@
 extends Node2D
 
 var letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
-var radius = 200
-var square_size = 80
+var radius = 300
+var square_size = 256
 
 func _ready():
+	get_tree().root.size_changed.connect(_on_viewport_size_changed)
+	update_letters()
+
+func _on_viewport_size_changed():
+	update_letters()
+
+func update_letters():
 	var num_letters = len(letters)
 	var angle_step = 2 * PI / num_letters
 	var screen_center = get_viewport_rect().get_center()
+	
+	for child in get_children():
+		child.queue_free()
 	
 	for i in range(num_letters):
 		var angle = i * angle_step
 		var x = cos(angle) * radius
 		var y = sin(angle) * radius
 		
-		var square = ColorRect.new()
-		square.color = Color.WHITE
-		square.set_size(Vector2(square_size, square_size))
-		square.set_position(screen_center + Vector2(x - square_size / 2, y - square_size / 2))
+		var sprite = Sprite2D.new()
+		sprite.texture = load("res://assets/alpha_tiles/letter_" + letters[i] + ".png")
+		var sprite_scale = Vector2(square_size / sprite.texture.get_width(), square_size / sprite.texture.get_height())
+		sprite.set_scale(sprite_scale)
 		
-		add_child(square)
+		var sprite_size = sprite.texture.get_size() * sprite_scale
+		sprite.set_position(screen_center + Vector2(x, y))
+		
+		add_child(sprite)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
