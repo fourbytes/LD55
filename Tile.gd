60003294bd6772b274bb840dc7639b4222aa7dba
ed.software <8016457+ed-software@users.noreply.github.com>
2024-04-13 16:38:25 +1000

    simplify tile class

--- Tile.gd
+++ Tile.gd
@@ -1,10 +1,7 @@
 class_name Tile
 extends Node
 
-const MAX_INT = 9223372036854775807
-
 var letter: String
-var id: int
 var isSelected: bool = false
 
 func _init():
@@ -12,7 +9,6 @@ func _init():
 	rng.randomize()  # Ensure a different seed each time the game starts
 	var ascii_value = rng.randi_range(65, 90)  # ASCII values from 65 to 90
 	letter = char(ascii_value)
-	id = randi()
 
 func select():
 	isSelected = true
